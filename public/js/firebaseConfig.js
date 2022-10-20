64bf0458058f29e51c21e4307342502f06bc1454
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2022-10-20 15:37:52 -0300

    firebase

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBpwbHoieDnt6msEAkud0FDoZH2wUucOuA",
+  authDomain: "projeto-python-44ade.firebaseapp.com",
+  projectId: "projeto-python-44ade",
+  storageBucket: "projeto-python-44ade.appspot.com",
+  messagingSenderId: "296232574311",
+  appId: "1:296232574311:web:f0eec2d6e175b139b163c2"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
