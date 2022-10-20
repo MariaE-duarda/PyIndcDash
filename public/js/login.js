f17da121cb698a07afd9d652999ad638f886db8f
Ravel Soares <ravelsoares64@gmail.com>
2022-10-20 16:15:11 -0300

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,11 +2,11 @@ function login() {
     var email = document.getElementById('email').value
     var senha = document.getElementById('senha').value
 
-    // Firebase.auth().signInWithEmailAndPassword(email, senha)
-    //     .then((_) => {
-    //         console.log('Login feito com sucesso')
-    //     })
-    //     .catch((error) => {
-    //         console.log('error', error.code)
-    //     });
+    firebase.auth().signInWithEmailAndPassword(email, senha)
+         .then((_) => {
+             console.log('Login feito com sucesso')
+         })
+         .catch((error) => {
+             console.log('error', error.code)
+         });
 }
