cd47a734fedc235b76b93dbca256ff3704d76c64
Ravel Soares <ravelsoares64@gmail.com>
2022-10-20 16:30:41 -0300

    app inicializado

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -6,6 +6,4 @@ const firebaseConfig = {
   storageBucket: "projeto-python-44ade.appspot.com",
   messagingSenderId: "296232574311",
   appId: "1:296232574311:web:f0eec2d6e175b139b163c2"
-}; 
-
-const app = initializeApp(firebaseConfig);
+};
