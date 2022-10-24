ed83f8b3148648f436da6a6d0c5fe58f6eddcb84
Ravel Soares <ravelsoares64@gmail.com>
2022-10-24 14:26:13 -0300

    puxando dados firestore

--- public/js/login.js
+++ public/js/login.js
@@ -4,9 +4,19 @@ function login() {
     firebase.auth().signInWithEmailAndPassword(email, senha)
         .then((_) => {
             console.log('Login feito com sucesso')
+
         })
         .catch((error) => {
             console.log('error', error.code)
         });
 }
+pegarDados();
+async function pegarDados() {
 
+    firebase.firestore().collection('ATIVOSDERESERVA').get().then(snapshot => {
+        console.log('snap')
+        console.log(snapshot)
+        var dados = snapshot.docs.map(doc => doc.data())
+        console.log(dados)
+    })
+}
