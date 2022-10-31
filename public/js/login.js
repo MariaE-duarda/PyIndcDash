6696b0fd94af28d33d1e19fdeb964f1deba62cc4
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2022-10-31 17:56:57 -0300

    responsividade (parcial)

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,19 @@
+firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+        window.location.href = 'main.html';
+    }
+})
+
 function login() {
     var email = document.getElementById('email').value
     var senha = document.getElementById('senha').value
     firebase.auth().signInWithEmailAndPassword(email, senha)
         .then((_) => {
-            console.log('Login feito com sucesso')
+            window.location.replace('main.html')
 
         })
         .catch((error) => {
             console.log('error', error.code)
+
         });
 }
-
